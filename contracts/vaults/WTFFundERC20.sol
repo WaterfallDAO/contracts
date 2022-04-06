e84c72824a6e6eb602171cc566da24acfd55a636
waterfall <waterfallDAO01@gmail.com>
2022-04-06 14:11:35 +0800

    Burn token

--- contracts/vaults/WTFFundERC20.sol
+++ contracts/vaults/WTFFundERC20.sol
@@ -7,13 +7,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract WTFFundERC20 is ERC20, Ownable {
 
     constructor(string memory name, string memory symbol, uint8 decimals_) public ERC20(name, symbol) {
-//        _setupDecimals(decimals_);
+        //        _setupDecimals(decimals_);
     }
     function mint(address _to, uint256 _amount) onlyOwner external {
         _mint(_to, _amount);
     }
 
-    function burn(address _to, uint256 _amount) external {
-        _burn(_to, _amount);
+    function burn(uint256 _amount) external {
+        _burn(msg.sender, _amount);
     }
 }
