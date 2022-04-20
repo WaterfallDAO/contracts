14565d5dbd23e877d10006b24bf5b9f9f5e1898f
waterfall <waterfallDAO01@gmail.com>
2022-04-20 09:46:18 +0800

    Move interfaces

--- contracts/mock/MockVault.sol
+++ contracts/mock/MockVault.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../interfaces/IVault.sol";
+import "../../interfaces/IVault.sol";
 
 contract MockVault is IVault {
 
--- contracts/vaults/Fof.sol
+++ contracts/vaults/Fof.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "../interfaces/IVault.sol";
+import "../../interfaces/IVault.sol";
 
 
 contract Fof {
