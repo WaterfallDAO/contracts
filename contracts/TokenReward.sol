11d78e9f465a572a4a28de6e05ebe87c11f72c9c
waterfall <waterfallDAO01@gmail.com>
2022-05-07 12:02:12 +0800

    Add MaterChef

--- contracts/TokenReward.sol
+++ contracts/TokenReward.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-import "../lib/TransferHelper.sol";
+import "./lib/TransferHelper.sol";
 import "../interfaces/IMinterToken.sol";
 import "./Operatable.sol";
 
